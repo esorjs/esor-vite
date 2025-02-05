c4a63afb67985888a73c2043b9033845366b26b5
juancristobalgd1 <juancristobalgd1@gmail.com>
2025-02-05 19:08:03 +0100

    Actualizar versión a 0.0.4, mejorar estilos y eliminar archivos obsoletos

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-import "./style.css";
+import "./App.css";
+import "./counter.js";
 import esorLogo from "/esor.svg";
 import viteLogo from "/vite.svg";
-import "./counter.js";
 
 document.querySelector("#app").innerHTML = `
     <div>
-        <a href="https://vite.dev" target="_blank">
+      <a href="https://vite.dev" target="_blank">
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://github.com/esorjs/esor" target="_blank">
+      </a>
+      <a href="https://github.com/esorjs/esor" target="_blank">
         <img src="${esorLogo}" class="logo esor" alt="Esor logo" />
-        </a>
-    <h1>Vite + Esor</h1>
-    <div class="card">
-      <my-counter></my-counter>
+      </a>
+      <h1>Vite + Esor</h1>
+       <my-counter></my-counter>
+      <p class="read-the-docs">
+        Click on the Vite and Esor logos to learn more
+      </p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
 `;
